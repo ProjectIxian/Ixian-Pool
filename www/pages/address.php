535d842eaf0f81afeec73a72711c39bdbd0ca347
Cristian Ciorba <cristian@firestorm.ro>
2019-05-16 22:29:04 +0300

    - added address and home page scripts

---
+++ www/pages/address.php
@@ -0,0 +1,61 @@
+<?php
+/*
+* Ixian Mining Pool
+* Website: www.ixian.io 
+* Github:  https://github.com/ProjectIxian/Ixian-Pool
+*/
+
+$page = new Template();
+
+$addressid = "";
+if(isset($_GET['id']) && $_GET['id'] != "") {
+    $addressid = $_GET['id'];
+    if(!ctype_alnum($addressid))
+    {
+        $page->render('page_error.tpl');
+        return;
+    }
+}
+else 
+{
+    $page->render('page_error.tpl');
+    return;
+}
+
+$active_shares = 0;
+
+$page->address = $addressid;
+
+db_connect();
+$page->workercount = db_fetch("SELECT COUNT(*) FROM `workers` where wallet = :address AND updated >= NOW() - INTERVAL 24 HOUR", array(':address' => $addressid))[0]['COUNT(*)'];
+
+if($page->workercount == 0)
+{
+    $page->render('page_error.tpl');
+    return;    
+}
+
+$miner = db_fetch("SELECT * from `miners` where address = :address LIMIT 1", array(':address' => $addressid));
+if(isset($miner[0]))
+{
+    $page->totalpaid = number_format($miner[0]['ixi_paid'],4);
+    $page->pending = number_format($miner[0]['ixi_pending'],4);
+    $active_shares = $miner[0]['shares'];
+}
+else
+{
+    $page->totalpaid = "0.0000";
+    $page->pending = "0.0000";   
+}
+
+
+$page->hashrate = db_fetch("SELECT sum(hashrate) as hashes FROM `workers` WHERE wallet = :address AND updated >= NOW() - INTERVAL 24 HOUR", array(':address' => $addressid))[0]['hashes']." h/s";
+
+if($page->hashrate == " h/s") {
+    $page->hashrate = "0 h/s";
+}
+
+
+$page->render('page_address.tpl');
+
+?>
---
+++ www/pages/home.php
@@ -0,0 +1,54 @@
+<?php
+/*
+* Ixian Mining Pool
+* Website: www.ixian.io 
+* Github:  https://github.com/ProjectIxian/Ixian-Pool
+*/
+
+$page = new Template();
+
+$blockdata = file_get_contents("cache/block.ixi");
+$response = json_decode($blockdata, true, 512, JSON_BIGINT_AS_STRING);
+
+$blockheight = "0";
+$page->blockheight = "0";
+$page->difficulty = "0";
+$page->reward = "0";
+$page->hashrate = "0";
+
+if(isset($response["result"]))
+{
+    $blockheight = $response["result"]["num"];
+    $page->difficulty = $response["result"]["dif"];
+}
+
+$page->blockheight = number_format($blockheight);
+
+db_connect();
+$notices = db_fetch("SELECT * from `notices` where visible = 1 ORDER BY date DESC", array());
+
+$page->noticecount = count($notices);
+$noticesarr = array();
+
+for($i = 0; $i < $page->noticecount; $i++) {
+    $noticesarr[$i]["message"] = $notices[$i]["message"];
+    if($notices[$i]["warning"] == 1) {
+        $noticesarr[$i]["warning"] = "callout-danger";
+    }
+    else {
+         $noticesarr[$i]["warning"] = "callout-info";
+    }
+}
+
+$page->notices=$noticesarr;
+
+
+
+$page->hashrate = db_fetch("SELECT sum(hashrate) as hashes FROM `workers` WHERE updated >= NOW() - INTERVAL 24 HOUR", array())[0]['hashes'];
+
+
+$page->reward = number_format(calculateRewardForBlock($blockheight),4);
+
+$page->render('page_home.tpl');
+
+?>
