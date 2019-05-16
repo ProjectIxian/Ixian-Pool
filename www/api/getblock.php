df601ba0cf2388df380197f7d70d7aa50c0e1b99
Cristian Ciorba <cristian@firestorm.ro>
2019-05-16 18:51:15 +0300

    - added api script files

---
+++ www/api/getblock.php
@@ -0,0 +1,33 @@
+<?php
+/*
+*   Client requests a new mining block
+*/
+header('Content-type: application/json');
+
+readfile("../cache/block.ixi");
+
+include_once("../config.php"); 
+
+if(!isset($_GET['wallet']) || !isset($_GET['worker']))
+{
+    die("Invalid parameters");
+}
+
+$wallet = $_GET['wallet'];
+$worker = $_GET['worker'];
+$id = md5($wallet.$worker);
+
+$hashrate = 0;
+if(isset($_GET['hr']))
+{
+    $hashrate = $_GET['hr'];
+}
+
+if(!ctype_alnum($wallet) || !ctype_alnum($worker) || !ctype_alnum($hashrate))
+    api_error("Incorrect parameters");
+
+db_connect();
+$statement = db_prepare("INSERT INTO `workers` (`id`,`wallet`, `name`, `hashrate`, `updated`) VALUES (:id, :address, :name, :hashrate, CURRENT_TIMESTAMP) ON DUPLICATE KEY UPDATE updated=CURRENT_TIMESTAMP, hashrate=:hashrate");
+db_execute($statement, array(':id' => $id, ':address' => $wallet, ':name' => $worker, ':hashrate' => $hashrate)); 
+
+?>
---
+++ www/api/submitminingsolution.php
@@ -0,0 +1,66 @@
+<?php
+
+include_once("../config.php"); 
+
+if(!isset($_GET['nonce']) || !isset($_GET['blocknum']) || !isset($_GET['wallet']) || !isset($_GET['worker'])) {
+    api_error("Invalid parameters");
+}
+
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$nonce = $_GET['nonce'];
+$blocknum = $_GET['blocknum'];
+
+$wallet = $_GET['wallet'];
+$worker = $_GET['worker'];
+
+if(!ctype_alnum($wallet) || !ctype_alnum($worker) || !ctype_alnum($blocknum) || !ctype_alnum($nonce))
+    api_error("Incorrect parameters");
+
+db_connect();
+$chk = db_fetch("SELECT count(1) FROM nonces WHERE nonce=:nonce", [":nonce" => $nonce])[0]['count(1)'];
+if ($chk != 0) {
+    api_error("Duplicate nonce");
+}
+
+
+$blockdatacontents = file_get_contents("../cache/block.ixi");
+$blockdata = json_decode($blockdatacontents, true, 512, JSON_BIGINT_AS_STRING);
+
+if(!isset($blockdata["result"])) {
+    api_error("Error");
+}
+
+
+$blockheight = $blockdata["result"]["num"];
+$difficulty = $blockdata["result"]["dif"];
+
+if($blockheight != $blocknum) {
+    api_error("Invalid blocknum");
+}
+
+
+$res = file_get_contents($dlt_host."/verifyminingsolution?nonce=$nonce&blocknum=$blocknum&diff=$difficulty");
+$data = json_decode($res, true);
+
+if(isset($data["error"]) || $data["result"] == "") {
+    api_error("Rejected");
+}
+
+$share = 1;
+
+db_fetch("INSERT IGNORE into nonces SET nonce=:nonce", [":nonce" => $nonce]);
+
+db_fetch("INSERT INTO miners SET address=:id, shares=shares+:sh, updated=CURRENT_TIMESTAMP ON DUPLICATE KEY UPDATE shares=shares+:sh2, updated=CURRENT_TIMESTAMP",
+            [":id" => $wallet, ":sh" => $share, ":sh2" => $share]
+        );
+
+
+$res = file_get_contents($pooladdress."/submitminingsolution?nonce=$nonce&blocknum=$blocknum", false, $context);
+$data = json_decode($res, true);
+
+if(isset($data["error"]) || $data["result"] == "") {
+    // Rejected with normal difficulty
+}
+
+?>
