7d9f792a7ef3350eeaada45d123a098e3e4e17c7
Cristian Ciorba <cristian@firestorm.ro>
2019-05-16 22:32:20 +0300

    - added index page

---
+++ www/index.php
@@ -0,0 +1,33 @@
+<?php
+/*
+* Ixian Mining Pool
+* Website: www.ixian.io 
+* Github:  https://github.com/ProjectIxian/Ixian-Pool
+*/
+
+include_once("config.php");
+include_once("include/template.php");
+
+$current_page = "home";
+
+if(isset($_GET['p']))
+{
+    $new_page = $_GET['p'];
+    
+    if($new_page === 'address' )
+    {
+        $current_page = $new_page;
+    }
+}
+
+$page = new Template();
+
+$page->cpage = $current_page;
+
+$page->render('header.tpl'); 
+
+include_once('pages/'.$current_page.'.php');
+
+$page->render('footer.tpl');
+
+?>
