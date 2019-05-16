3e28fe77b1b7edb8de0da1415e12ba74d12fe276
Cristian Ciorba <cristian@firestorm.ro>
2019-05-16 22:31:47 +0300

    - added maintenance scripts

---
+++ www/scripts/balances.php
@@ -0,0 +1,49 @@
+<?php
+/*
+* Ixian Mining Pool
+* Website: www.ixian.io 
+* Github:  https://github.com/ProjectIxian/Ixian-Pool
+*/
+
+include_once("../config.php");
+
+$params = "/mywallet";
+
+$response = file_get_contents($dlt_host.$params);
+$response = json_decode($response, true, 512, JSON_BIGINT_AS_STRING);
+
+if(!isset($response["result"]))
+    api_error("Can't retrieve balance");
+
+$primarybalance = reset($response["result"]);
+if($primarybalance == FALSE)
+    api_error("Incorrect balance");
+
+if($primarybalance < 10)
+    api_error("Balance too low for payouts");
+
+db_connect();
+
+$totalshares = db_fetch("SELECT sum(shares) as sh FROM `miners` WHERE shares > 0", array())[0]['sh'];
+
+if($totalshares < 1)
+    api_error("Not enough shares to issue payouts");
+
+echo "Total shares: $totalshares | ";
+
+$primarybalance = $primarybalance - $poollocked_balance;
+$fee = $primarybalance * $poolfee;
+$primarybalance = $primarybalance - $fee;
+$rewardpershare = $primarybalance / $totalshares;
+
+echo "Reward per share: $rewardpershare";
+
+$miners = db_fetch("SELECT * from `miners` where shares > 0", array());
+
+foreach($miners as $miner)
+{
+    $pending = $rewardpershare * $miner["shares"];
+    db_fetch("UPDATE `miners` SET `ixi_pending` = :pending WHERE `miners`.`address` = :address", [":pending" => $pending, "address" => $miner["address"]]);
+}
+
+?>
---
+++ www/scripts/fetch.php
@@ -0,0 +1,34 @@
+<?php
+/*
+* Ixian Mining Pool
+* Website: www.ixian.io 
+* Github:  https://github.com/ProjectIxian/Ixian-Pool
+*/
+
+include_once("../config.php");
+
+$params = "/getminingblock?algo=0";
+
+$response = file_get_contents($dlt_host.$params);
+$response = json_decode($response, true, 512, JSON_BIGINT_AS_STRING);
+
+$cache_file = "../cache/block.ixi";
+
+$num = $response["result"]["num"];
+$version = $response["result"]["ver"];
+$diff = $response["result"]["dif"];
+$checksum = $response["result"]["chk"];
+$address = $response["result"]["adr"];
+
+db_connect();
+db_fetch("INSERT INTO `blocks` (`num`, `version`, `diff`, `checksum`, `address`) VALUES (:num, :version, :diff, :checksum, :address)", [":num" => $num, ":version" => $version, ":diff" => $diff, ":checksum" => $checksum, ":address" => $address]);
+
+$response["result"]["dif"] = substr($diff, 0, $pooldifficulty_reduction);
+$response["result"]["PoW field"] = "";
+
+$new_data = json_encode($response);
+file_put_contents($cache_file, $new_data);
+
+echo json_encode($new_data);
+
+?>
